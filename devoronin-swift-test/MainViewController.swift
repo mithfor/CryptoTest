70b896a2838ccb75e88380e43e163db5f586139c
Dmitriy Voronin <devoronin@gmail.com>
2023-02-25 21:43:06 +0300

    Tabbar with NavigationControllers were created

---
+++ devoronin-swift-test/MainViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  devoronin-swift-test
+//
+//  Created by Dmitrii Voronin on 17.02.2023.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configure()
+        
+    }
+    
+    fileprivate func configure() {
+        
+        createTabBar()
+    }
+    
+    private func createTabBar() {
+        
+        viewControllers = [
+            createAssetsNavigationConroller(),
+            createWatchlistNavigationController(),
+            createSettingsNavigationController()]
+
+    }
+    
+    
+    private func createAssetsNavigationConroller() -> UINavigationController {
+        
+        let assetsVC = AssetsViewController()
+        assetsVC.title = "Assets"
+        assetsVC.tabBarItem = UITabBarItem(title: assetsVC.title,
+                                           image: UIImage(systemName: "bitcoinsign.circle.fill"),
+                                           tag: 0)
+        return UINavigationController(rootViewController: assetsVC)
+    }
+    
+    private func createWatchlistNavigationController() -> UINavigationController {
+        let watchlistVC = WatchlistViewController()
+        watchlistVC.title = "Watchlist"
+        watchlistVC.tabBarItem = UITabBarItem(title: watchlistVC.title,
+                                              image: UIImage(systemName: "heart.fill"),
+                                              tag: 1)
+        return UINavigationController(rootViewController: watchlistVC)
+    }
+    
+    private func createSettingsNavigationController() -> UINavigationController {
+        let settingsVC = SettingsViewController()
+        settingsVC.title = "Settings"
+        settingsVC.tabBarItem = UITabBarItem(title: settingsVC.title,
+                                             image: UIImage(systemName: "gearshape.fill"),
+                                             tag: 2)
+        return UINavigationController(rootViewController: settingsVC)
+    }
+}
+
